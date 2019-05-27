ad45cc3de7825f5a0ebb94cc45af90f5ea469075
IzabelaSkoczen94 <izabela.s94@gmail.com>
2019-05-27 10:09:15 +0200

    Updates

---
+++ precache-manifest.5b5021da003ffc2fd81c183d1a14e383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59df28fe2d9725fadcb0d8a91669ab1b",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "d8e31e25cc620e936bd3",
+    "url": "/reactapp/static/css/main.390d3f23.chunk.css"
+  },
+  {
+    "revision": "775e76d0d133815d5c11",
+    "url": "/reactapp/static/js/2.edd8c730.chunk.js"
+  },
+  {
+    "revision": "d8e31e25cc620e936bd3",
+    "url": "/reactapp/static/js/main.76e97875.chunk.js"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  }
+]);
