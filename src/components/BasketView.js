aa274afd3be4b5b6fa6e0f28e360c9e20e18fc6c
IzabelaSkoczen94 <izabela.s94@gmail.com>
2019-05-27 09:23:29 +0200

    project

---
+++ src/components/BasketView.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+class BasketView extends React.Component {
+  render() {
+    return (
+      <>
+        <div id="cart" />
+        {this.props.products.map(item => {
+          return (
+            <div key={item.id + 10}>
+              <ul className="user-cart--list">
+                <li>
+                  <button key={item.id} onClick={() => this.props.deleteItem(item.id)}>x</button>
+                  {item.id - 1} - {item.name} - {item.price} zl
+                </li>
+              </ul>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default BasketView;
